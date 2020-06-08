4ca26a9d179ca9b12d3b57e8736114f59b43d47a
Allan Cheng <allanhfcheng@gmail.com>
2020-06-07 23:56:09 -0700

    trying to push

--- todo-router.js
+++ todo-router.js
@@ -1,40 +1,20 @@
-require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const errorHandler = require('./middleware/error-handler')
-const TodoService = require('./todo/todo-service')
-//const xss = require('xss')
 const jsonParser = express.json()
-//const path = require('path')
-const toDoRouter = require('../todo-router')
+const toDoRouter = express.Router()
+const xss = require('xss')
+const path = require('path')
+const TodoService = require('./src/todo/todo-service.js')
 
-const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption, {
-  skip: () => NODE_ENV === 'test',
-}))
-app.use(cors())
-app.use(helmet())
-
-app.use(express.static('public'))
-app.use(toDoRouter)
-/*
 const serializeTodo = todo => ({
-  id: todo.id,
-  title: xss(todo.title),
-  completed: todo.completed
-})
+    id: todo.id,
+    title: xss(todo.title),
+    completed: todo.completed
+  })
 
-app
-  .route('/v1/todos')
-  .get((req, res, next) => {
+toDoRouter
+.route('/v1/todos')
+.get((req, res, next) => {
     const knexInstance = req.app.get('db')
     TodoService.getTodos(knexInstance)
       .then(todos => {
@@ -67,7 +47,7 @@ app
       .catch(next)
   })
 
-app
+toDoRouter
   .route('/v1/todos/:todo_id')
   .all((req, res, next) => {
     if(isNaN(parseInt(req.params.todo_id))) {
@@ -125,8 +105,5 @@ app
       })
       .catch(next)
   })
-*/
 
-app.use(errorHandler)
-
-module.exports = app
+  module.exports = toDoRouter
